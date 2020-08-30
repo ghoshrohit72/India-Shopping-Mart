fbabb38c7cdff098767eeb0043724dcdd1e84676
ghoshrohit72 <ghoshrohit72@gmail.com>
2020-08-30 23:18:40 +0530

    Added Navbar,footer and basic functionality

--- India_Bazaar_Project/store/urls.py
+++ India_Bazaar_Project/store/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('product/', views.productPage, name='product'),
 ]
