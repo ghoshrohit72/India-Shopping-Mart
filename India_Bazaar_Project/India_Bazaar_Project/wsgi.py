215ee52c78058f8afb0424e07a02783952cc265f
ghoshrohit72 <ghoshrohit72@gmail.com>
2020-08-30 13:44:18 +0530

    Added Bootstrap and basic structure of the site

---
+++ India_Bazaar_Project/India_Bazaar_Project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for India_Bazaar_Project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'India_Bazaar_Project.settings')
+
+application = get_wsgi_application()
